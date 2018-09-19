67591ab26a40bec42e981456f69314f3ce2eabd5
Eric <coolspot1000@gmail.com>
2018-09-18 23:01:06 -0500

    Cleaned up

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var logger = require('morgan');
 var errorHandler = require('errorhandler');
 var dotenv = require('dotenv');
 
-var flash = require('express-flash');
 var path = require('path');
 var expressValidator = require('express-validator');
 var multer = require('multer');
@@ -32,19 +31,6 @@ var express = require('express'),
    server = http.createServer(app),
    io = require('socket.io').listen(server);
 server.listen(7000);
-/**
- * Controllers (route handlers).
- */
-var homeController = require('./controllers/home');
-var userController = require('./controllers/user');
-var apiController = require('./controllers/api');
-var contactController = require('./controllers/contact');
-var adminController = require('./controllers/admin');
-
-/**
- * API keys and Passport configuration.
- */
-var passportConfig = require('./config/passport');
 
 /**
  * Express configuration.
@@ -58,20 +44,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-app.use(function(req, res, next) {
-  res.locals.user = req.user;
-  next();
-});
-app.use(function(req, res, next) {
-  // After successful login, redirect back to /api, /contact or /
-  if (/(api)|(contact)|(^\/$)/i.test(req.path)) {
-    req.session.returnTo = '/';
-  }
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.locals.user = req.user;
+//   next();
+// });
+// app.use(function(req, res, next) {
+//   // After successful login, redirect back to /api, /contact or /
+//   if (/(api)|(contact)|(^\/$)/i.test(req.path)) {
+//     req.session.returnTo = '/';
+//   }
+//   next();
+// });
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
 var needsUsername = function(username) {
@@ -87,27 +70,9 @@ var needsUsername = function(username) {
 /**
  * Primary app routes.
  */
-app.get('/', homeController.index)
-app.get('/login', userController.getLogin);
-app.post('/login', userController.postLogin);
-app.get('/logout', userController.logout);
-app.get('/forgot', userController.getForgot);
-app.post('/forgot', userController.postForgot);
-app.get('/reset/:token', userController.getReset);
-app.post('/reset/:token', userController.postReset);
-app.get('/signup', userController.getSignup);
-app.post('/signup', userController.postSignup);
-app.get('/contact', contactController.getContact);
-app.post('/contact', contactController.postContact);
-app.get('/account/dashboard', passportConfig.isAuthenticated, userController.getDashboard);
-app.get('/account/profile', passportConfig.isAuthenticated, userController.getAccount);
-app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
-app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
-app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
-app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
-app.get('/admin', needsUsername('admin'), adminController.index);
-
-var User = require('./models/User');
+app.get('/', function(req,res){
+  res.render('home');
+})
 
 
 
@@ -116,7 +81,9 @@ var User = require('./models/User');
  */
 app.use(errorHandler());
 
-
+/*
+* Socket IO for future use
+*/
 io.on('connection', function (socket) {
 
 
